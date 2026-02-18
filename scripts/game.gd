cb5d3f2a0fcc5687e0c04dfb1f6122348c82b693
Robin PM <dev@robinpm.com>
2026-02-18 00:33:39 -0800

    murdering friendly clone

--- scripts/game.gd
+++ scripts/game.gd
@@ -11,15 +11,18 @@ func load_dream_scene(scene: PackedScene) -> void:
 	var scene_instance = scene.instantiate()
 	get_tree().current_scene.add_child(scene_instance)
 	
-	# Instantiate and add the player
-	var player_scene = load("res://scenes/player.tscn")
-	var player_instance = player_scene.instantiate()
-	get_tree().current_scene.add_child(player_instance)
-
-	# Position player at SpawnPoint if one exists
+	# Position player at SpawnPoint if one exists, or spawn one if the scene doesn't have one
 	var spawn = scene_instance.find_child("SpawnPoint")
-	if spawn:
-		player_instance.global_position = spawn.global_position
+	var existing_player = scene_instance.find_child("Player")
+	if existing_player:
+		if spawn:
+			existing_player.global_position = spawn.global_position
+	else:
+		var player_scene = load("res://scenes/player.tscn")
+		var player_instance = player_scene.instantiate()
+		get_tree().current_scene.add_child(player_instance)
+		if spawn:
+			player_instance.global_position = spawn.global_position
 
 func help_me() ->  void:
 	get_tree().quit()
